a3ede7af308a915bd47232b82ec7bfc8522860a7
liwenhao <2739227392@qq.com>
2019-01-05 16:35:54 +0800

    对象克隆 Cloneable 接口

---
+++ src/cn/haohaoli/book/core/chapter5/interfaces/CloneTest.java
@@ -0,0 +1,21 @@
+package cn.haohaoli.book.core.chapter5.interfaces;
+
+/**
+ * @author liWenHao
+ * @date 2019/1/5 15:56
+ **/
+public class CloneTest {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Employee c = new Employee("c", 75000);
+        c.setHireDay(2019, 1, 1);
+        Employee copy = c.clone();
+        c.setName("x");
+        c.raiseSalary(100d);
+        c.setHireDay(2018, 1, 1);
+        System.out.println("c= " + c);
+        System.out.println("copy= " + copy);
+        int[] x = {1, 2, 3, 4, 5};
+        int[] clone1 = x.clone();
+    }
+}
---
+++ src/cn/haohaoli/book/core/chapter5/interfaces/Employee.java
@@ -0,0 +1,77 @@
+package cn.haohaoli.book.core.chapter5.interfaces;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * 雇员类
+ */
+public class Employee implements Cloneable {
+
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        hireDay = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Date getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(int year,int month,int day) {
+        Date time = new GregorianCalendar(year, month - 1, day).getTime();
+        hireDay.setTime(time.getTime());
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 10;
+        salary += raise;
+    }
+
+
+    /**
+     *  单独使用 Employee clone = (Employee) super.clone();
+     *      可能对象中存在对其他类的引用
+     * @return
+     * @throws CloneNotSupportedException
+     */
+    @Override
+    protected Employee clone() throws CloneNotSupportedException {
+        //深拷贝例子
+        Employee clone = (Employee) super.clone();
+        clone.hireDay = (Date) hireDay.clone();
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", hireDay=" + hireDay +
+                '}';
+    }
+}
