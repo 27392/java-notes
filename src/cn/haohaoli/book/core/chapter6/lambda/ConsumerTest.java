282d5ff36f0bc23cc2d132ba69bd2c3586dd3744
李文昊 <2739227392@qq.com>
2019-01-18 21:19:40 +0800

    Consumer接口

--- src/cn/haohaoli/book/core/chapter6/lambda/ConsumerTest.java
+++ src/cn/haohaoli/book/core/chapter6/lambda/ConsumerTest.java
@@ -1,5 +1,12 @@
 package cn.haohaoli.book.core.chapter6.lambda;
 
+import cn.haohaoli.book.core.chapter6.lambda.factory.Tiger;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.Predicate;
+
 /**
  * @author liWenHao
  * @date 2019/1/7 11:22
@@ -8,7 +15,26 @@ public class ConsumerTest {
 
     //Consumer<T>     T        处理一个T类型的值
     public static void main(String[] args) {
+        repeat(10, System.out::println);
 
+        Tiger animal = new Tiger();
+        animal.setName("小白");
+
+        changeColor(animal, tiger -> tiger.getName().equals("小白"), System.out::println);
+
+    }
+
+    public static void repeat(int i, IntConsumer intConsumer) {
+        for (int j = 0; j < i; j++) {
+            intConsumer.accept(j);
+        }
+    }
+
+    public static Tiger changeColor(Tiger t, Predicate<Tiger> predicate, Consumer<Tiger> consumer) {
+        if (predicate.test(t)) {
+            consumer.accept(t);
+        }
+        return t;
     }
 
 }
--- src/cn/haohaoli/book/core/chapter6/lambda/factory/Tiger.java
+++ src/cn/haohaoli/book/core/chapter6/lambda/factory/Tiger.java
@@ -5,4 +5,32 @@ package cn.haohaoli.book.core.chapter6.lambda.factory;
  * @date 2019/1/7 11:01
  */
 public class Tiger extends Animal {
+
+    private String name;
+
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Tiger{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
