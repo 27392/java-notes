ea671d4d3fe1411e268029e8994d00ae6dafcd10
liwenhao <2739227392@qq.com>
2019-01-23 14:03:34 +0800

    添加LocalDate注释

--- src/cn/haohaoli/book/core/chapter4/localDateTest.java
+++ src/cn/haohaoli/book/core/chapter4/localDateTest.java
@@ -3,16 +3,27 @@ package cn.haohaoli.book.core.chapter4;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
+/**
+ * java8 LocalDate类
+ * @see LocalDate
+ * @author liWenHao
+ * @date 2019/1/12 09:53
+ */
 public class localDateTest {
 
     public static void main(String[] args) {
 
+        /**
+         * 不要使用构造器来构造 LocalDate 类的对象。实际上，应当使用静态工厂方法 (factory method) 代表你调用构造器。
+         */
+        LocalDate now = LocalDate.now();
+        print(now);
+        LocalDate newYear = LocalDate.of(2020, 11, 11);
+        print(newYear);
+        print(newYear.plusYears(100));
         LocalDate date = LocalDate.now();
-        //月
         int month = date.getMonthValue();
-        //日
         int today = date.getDayOfMonth();
-
         //设置时间为本月一号
         date = date.minusDays(today - 1);
 
@@ -35,5 +46,26 @@ public class localDateTest {
             if (date.getDayOfWeek().getValue() == 1) System.out.println();
         }
 
+    }
+
+    private static void print(LocalDate date) {
+        //获取年
+        int year = date.getYear();
+        //获取月份
+        int monthValue = date.getMonthValue();
+        //获取日
+        int dayOfMonth = date.getDayOfMonth();
+        /**
+         * https://www.cnblogs.com/seakt/p/4478045.html
+         *      %c        单个字符
+         *      %d        十进制整数
+         *      %f        十进制浮点数
+         *      %o        八进制数
+         *      %s        字符串
+         *      %u        无符号十进制数
+         *      %x        十六进制数
+         *      %%        输出百分号%
+         */
+        System.out.printf("%d - %d - %d \n", year, monthValue, dayOfMonth);
     }
 }
