99bedc47779439eae8ea9960b8adf7e7cdae752e
liwenhao <2739227392@qq.com>
2019-02-05 21:25:32 +0800

    静态代理和动态代理

---
+++ src/cn/haohaoli/book/core/chapter6/proxy/dynamicProxy/DynamicProxyTest.java
@@ -0,0 +1,21 @@
+package cn.haohaoli.book.core.chapter6.proxy.dynamicProxy;
+
+import cn.haohaoli.book.core.chapter6.proxy.Target;
+import cn.haohaoli.book.core.chapter6.proxy.TargetImpl;
+
+/**
+ * 代理测试类
+ * @author LiWenHao
+ * @date 2019-02-05 17:22
+ */
+public class DynamicProxyTest {
+
+    public static void main(String[] args) {
+//        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        Target target = new TargetImpl();
+        DynamicProxy dynamicProxy = new DynamicProxy(target);
+        Target o = (Target) dynamicProxy.newProxyInstance();
+        String execute = o.execute();
+    }
+}
---
+++ src/cn/haohaoli/book/core/chapter6/proxy/staticProxy/StaticProxyTest.java
@@ -0,0 +1,18 @@
+package cn.haohaoli.book.core.chapter6.proxy.staticProxy;
+
+import cn.haohaoli.book.core.chapter6.proxy.Target;
+import cn.haohaoli.book.core.chapter6.proxy.TargetImpl;
+
+/**
+ * @author LiWenHao
+ * @date 2019-02-05 20:16
+ */
+public class StaticProxyTest {
+
+    public static void main(String[] args) {
+
+        Target target = new TargetImpl();
+        Proxy proxy = new Proxy(target);
+        proxy.execute();
+    }
+}
