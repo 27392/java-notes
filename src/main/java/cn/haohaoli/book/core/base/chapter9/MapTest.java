84f64418171ecae429f7e389a41f8c70f452f331
liwenhao <2739227392@qq.com>
2019-05-27 20:51:44 +0800

    添加Map在JDK8中的操作

--- src/main/java/cn/haohaoli/book/core/base/chapter9/MapTest.java
+++ src/main/java/cn/haohaoli/book/core/base/chapter9/MapTest.java
@@ -5,6 +5,7 @@ import lombok.*;
 import java.util.*;
 
 /**
+ * 资料参考 ： https://www.cnblogs.com/CarpenterLee/p/6507161.html
  * @author LiWenHao
  * @date 2019-05-26 18:33
  */
@@ -43,6 +44,42 @@ public class MapTest {
         map.putAll(staff);
         map.forEach((k, v) -> System.out.printf("key = %s , value = %s\n", k, v));
 
+        System.out.println("=======");
+
+        Map<String, Integer> m = new HashMap<>();
+        m.put("1", 1);
+        m.put("2", 3);
+        m.put("3", 3);
+
+        //如果key空就put
+        m.putIfAbsent("1", 2);
+
+        //合并
+        // m.merge("1", 3, Integer::max);
+        m.merge("1", 3, (v1, v2) -> v1 > v2 ? v1 : v2);
+
+        //如果key 不存在就获取给定的值
+        System.out.println("getOrDefault : " + m.getOrDefault("99", 99));
+
+        //根据key 替换 value
+        m.replace("2", 2);
+
+        //key,value 替换value 返回 是否替换成功
+        boolean replace = m.replace("3", 3, 4);
+
+        //替换全部 value
+        m.replaceAll((k, v) -> v * 10);
+
+        //计算
+        m.compute("1", (k, v) -> v != null ? 100 : 1);
+
+        //如果不存在
+        m.computeIfAbsent("99", (v) -> 999);
+
+        //如果存在
+        m.computeIfPresent("99", (k, v) -> v / 10);
+
+        m.forEach((k, v) -> System.out.printf("key = %s , value = %s\n", k, v));
     }
 
 
