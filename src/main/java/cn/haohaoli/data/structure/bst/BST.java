a86a848d0d7062d10b75be02cbb727e1d1cf0f42
liwenhao <2739227392@qq.com>
2019-07-11 16:20:06 +0800

    数据结构: 二分搜索树-搜索元素

--- src/main/java/cn/haohaoli/data/structure/bst/BST.java
+++ src/main/java/cn/haohaoli/data/structure/bst/BST.java
@@ -24,6 +24,10 @@ public class BST<E extends Comparable<E>> {
         }
     }
 
+    /**
+     * 添加元素
+     * @param e
+     */
     public void add (E e) {
         root = add(root, e);
     }
@@ -47,6 +51,34 @@ public class BST<E extends Comparable<E>> {
             node.right = add(node.right, e);
         }
         return node;
+    }
+
+    /**
+     * 二分搜索数中是否保存元素
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    /**
+     * 向以node为根的二分搜索数中是否包含元素e, 递归算法
+     * @param node
+     * @param e
+     * @return
+     */
+    private boolean contains(Node node, E e) {
+        if (null == node) {
+            return false;
+        }
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
     }
 
     public int getSize() {
